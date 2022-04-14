15c79d446dba2cecb8a1712cba7497237f2eb623
Nathan Cai <wenjuncai97@gmail.com>
2022-04-13 21:50:50 -0400

    added custom hooks

---
+++ frontend/hooks/useDarkMode.js
@@ -0,0 +1,10 @@
+import {useState} from 'react';
+import useLocalStorage from './useLocalStorage';
+
+const useDarkMode = (initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage("darkmode", initialValue);
+
+    return [darkMode, setDarkMode]
+}
+
+export default useDarkMode;
